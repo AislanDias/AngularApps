e8b96e8305d424f233e1440687740ed6fb027c2a
Aislan Dias <aislandias@outlook.com>
2024-03-09 17:21:04 -0300

    [mock] mock functionality

--- SimpleApps/hotel-app/src/app/reservation-form/reservation-form.component.ts
+++ SimpleApps/hotel-app/src/app/reservation-form/reservation-form.component.ts
@@ -33,10 +33,10 @@ export class ReservationFormComponent implements OnInit{
     let id = this.activatedRoute.snapshot.paramMap.get('id')
 
     if(id){
-      let reservation = this.reservationService.getReservation(id)
-
-      if( reservation )
-        this.reservationForm.patchValue(reservation)
+      let reservation = this.reservationService.getReservation(id).subscribe(reservation => {
+        if( reservation )
+          this.reservationForm.patchValue(reservation)
+      })
     }
   }
 
@@ -48,10 +48,15 @@ export class ReservationFormComponent implements OnInit{
       let id = this.activatedRoute.snapshot.paramMap.get('id')
 
       if(id){
-        this.reservationService.updateReservation(id, reservation)
+        // this.reservationService.updateReservation(id, reservation)
+        this.reservationService.updateReservation(id, reservation).subscribe(() => {
+          console.log("Update request processed")
+        })
       }
       else{
-        this.reservationService.addReservation(reservation)
+        this.reservationService.addReservation(reservation).subscribe(() => {
+          console.log("Post request processed")
+        })
       }
 
       this.router.navigate(['/list'])
