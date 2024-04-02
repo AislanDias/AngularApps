15332da27d43a37df3da94aa560ca72d68f5dd68
Aislan Dias <aislandias@outlook.com>
2024-04-02 12:30:03 +0200

    [docs] start-routing#associate-a-url-path-with-a-component

--- Docs/0-getting-started/src/app/product-details/product-details.component.ts
+++ Docs/0-getting-started/src/app/product-details/product-details.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
 
+  product: Product | undefined;
+
+  // ActivatedRoute is specific to each component that the Angular Router loads. 
+  // ActivatedRoute contains information about the route and the route's parameters.
+  // By injecting ActivatedRoute, you are configuring the component to use a service. 
+  // The Managing Data step covers services in more detail.
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    // First get the product id from current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // Find the product that correspond with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute)
+  }
 }
